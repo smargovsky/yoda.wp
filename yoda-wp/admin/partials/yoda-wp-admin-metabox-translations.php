3d50f9ffae9030fc765569ca7f4352901d560c39
Brian M. Herold <brian.herold@genesys.com>
2018-07-27 15:52:44 -0500

    Added all translations

--- yoda-wp/admin/partials/yoda-wp-admin-metabox-translations.php
+++ yoda-wp/admin/partials/yoda-wp-admin-metabox-translations.php
@@ -12,18 +12,37 @@ if ( empty( $post ) ) { return; }
 if ( 'wizard' != $post->post_type && 'announcement' != $post->post_type ) { return; }
 $this->meta = get_post_meta( $post->ID, 'translations' );
 
-$langs = array( 'es', 'jp', 'no', 'ru', 'de', 'fr' );
+$langs = [
+    ['code' => 'da', 'display' => 'Danish'],
+    ['code' => 'de', 'display' => 'German'],
+    ['code' => 'en', 'display' => 'English'],
+    ['code' => 'es', 'display' => 'Spanish'],
+    ['code' => 'fi', 'display' => 'Finnish'],
+    ['code' => 'fr', 'display' => 'French'],
+    ['code' => 'it', 'display' => 'Italian'],
+    ['code' => 'ja', 'display' => 'Japanese'],
+    ['code' => 'ko', 'display' => 'Korean'],
+    ['code' => 'nl', 'display' => 'Dutch'],
+    ['code' => 'no', 'display' => 'Norwegian'],
+    ['code' => 'pl', 'display' => 'Polish'],
+    ['code' => 'pt-br', 'display' => 'Portuguese (Brazil)'],
+    ['code' => 'sv', 'display' => 'Swedish'],
+    ['code' => 'th', 'display' => 'Thai'],
+    ['code' => 'tr', 'display' => 'Turkish'],
+    ['code' => 'zh-cn', 'display' => 'Chinese (S)'],
+    ['code' => 'zh-tw', 'display' => 'Chinese (T)']
+];
 
 $availableLangs = is_array($this->db::getGuideAvailableTranslations($post)) ? array_keys($this->db::getGuideAvailableTranslations($post)) : [];
 
 foreach ($langs as $lang) {
-    if ( in_array($lang, $availableLangs) ) { ?>
+    if ( in_array($lang['code'], $availableLangs) ) { ?>
         <p>
-            <span class="dashicons dashicons-thumbs-up" style="color: #5cb85c"></span><strong style="color:#5cb85c"> <?php echo $lang; ?> </strong>
+            <span class="dashicons dashicons-thumbs-up" style="color: #5cb85c"></span><strong style="color:#5cb85c"> <?php echo $lang['display']; ?> </strong>
         </p>
     <?php } else { ?>
         <p>
-            <span class="dashicons dashicons-thumbs-down" style="color: red"></span><strong style="color:red"> <?php echo $lang; ?> </strong>
+            <span class="dashicons dashicons-thumbs-down" style="color: red"></span><strong style="color:red"> <?php echo $lang['display']; ?> </strong>
         </p>
     <?php }
 }
